4af314fe85cdcb4568360e39e13e2de9837d7f47
Eric Yu <yu.czeric@gmail.com>
2024-05-13 13:09:16 +0200

    adding mutex for concurrent access

--- shm/Channel.java
+++ shm/Channel.java
@@ -12,6 +12,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Channel<T> implements go.Channel<T> {
 
+    private boolean usedValue = true;
     LinkedList<T> listValues = new LinkedList<T>();
     final Lock lock = new ReentrantLock();
     final Condition notEmpty = lock.newCondition();
@@ -26,51 +27,55 @@ public class Channel<T> implements go.Channel<T> {
     }
 
     public void out(T v) {
-        // TODO: Appel Callback -> prevenir operation OUT
-        if (!observersOut.isEmpty()){
-            Iterator<Observer> iterator = observersOut.iterator();
-            // Iterate through the ArrayList and remove each item
-            while (iterator.hasNext()) {
-                Observer obs = iterator.next();
-                obs.update();
-                iterator.remove();
-            }
-        }
-
-        System.out.println("OUT: Starting out with value "+ v.toString() + " to list.");
         lock.lock();
+        System.out.println("OUT: Starting out with value "+ v.toString() + " to list.");
         try {
+            // TODO: Appel Callback -> prevenir operation OUT
+            if (!observersOut.isEmpty()){
+                Iterator<Observer> iterator = observersOut.iterator();
+                // Iterate through the ArrayList and remove each item
+                while (iterator.hasNext()) {
+                    Observer obs = iterator.next();
+                    obs.update();
+                    iterator.remove();
+                }
+            }
+            while(!usedValue)
+                waitingValue.await();
             System.out.println("OUT: Adding value "+ v.toString() + " to list.");
-
             listValues.add(v);
             System.out.println("OUT: Signaling NOTEMPTY");
             notEmpty.signal();
-        } finally {
+            usedValue=false;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
             lock.unlock();
         }
     }
 
     public T in() {
-        // TODO: Appel Callback -> prevenir operation IN
-        if (!observersIn.isEmpty()){
-            Iterator<Observer> iterator = observersIn.iterator();
-            // Iterate through the ArrayList and remove each item
-            while (iterator.hasNext()) {
-                Observer obs = iterator.next();
-                obs.update();
-                iterator.remove();
-            }
-        }
-
         lock.lock();
-        System.out.println("IN: Retrieving value");
-
         try {
-            while (listValues.isEmpty()) {
-                System.out.println("IN:  WAITINGVALUE");
-                notEmpty.await();
+            // TODO: Appel Callback -> prevenir operation IN
+            if (!observersIn.isEmpty()){
+                Iterator<Observer> iterator = observersIn.iterator();
+                // Iterate through the ArrayList and remove each item
+                while (iterator.hasNext()) {
+                    Observer obs = iterator.next();
+                    obs.update();
+                    iterator.remove();
+                }
             }
+
+            System.out.println("IN: Retrieving value");
+            while (usedValue)
+                notEmpty.await();
+            waitingValue.signalAll();
+            System.out.println("IN:  WAITINGVALUE");
             System.out.println("IN:  Removing value");
+            usedValue=true;
             return listValues.remove();
         } catch (InterruptedException e) {
             e.printStackTrace();
